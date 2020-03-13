332d72c4676a53345ab01f29b163e9c6a8036b60
thdotnet <thiago.custodio@hotmail.com>
2020-03-12 23:15:05 -0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Net.Client;
+using GrpcServer.Services;
+using Newtonsoft.Json;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new MessageService.MessageServiceClient(channel);
+
+            Console.WriteLine("Digite a mensagem");
+            var mensagem = Console.ReadLine();
+
+            var request = new SampleRequest
+            {
+                Conteudo = mensagem,
+                Id = 1
+            };
+
+            var response = client.Ping(request);
+
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ GrpcServer/Services/Service.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class Service : MessageService.MessageServiceBase
+    {
+        public override Task<SampleResponse> Ping(SampleRequest request, ServerCallContext context)
+        {
+            var response = new SampleResponse
+            {
+                Conteudo = $"{request.Conteudo}:{string.Join("", request.Conteudo.ToArray().Reverse())}",
+                Id = 7
+            };
+            return Task.FromResult(response);
+        }
+    }
+}
